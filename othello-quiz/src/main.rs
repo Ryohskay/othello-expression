7ec5cd97b9be0151ab320522536f69ee60bf72c0
Ryohskay on Pop <learnerryo@gmail.com>
2023-02-06 03:47:28 +0000

    playing around with binary operators

--- othello-quiz/src/main.rs
+++ othello-quiz/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     let four_in_b = 0b100; // always prepend 0b, just as you do for hex 0x
     println!("{}", four_in_b);
+    println!("0b100 >> 2 is 0b{:b}", 0b100 >> 2);
+    println!("0b10 << 5 is 0b{:b}", 0b10 << 5);
+    println!("0b101 << 2 is 0b{:b}", 0b101 << 2);
 }
